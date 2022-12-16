18d36e3c00811581ec92f75329e4edc38e5be316
Rémy <remyc59440@hotmail.com>
2022-12-16 13:36:22 +0100

    ok

---
+++ main.js
@@ -0,0 +1,10 @@
+$( "#myId" ).text("Hello World");
+$('#link').attr({
+    title: 'test',
+    href: "google.fr"
+})
+
+$( "a" ).attr( "href", 'google.fr');
+
+
+$('div').css("widht", "3rem").css("height", '3rem').css('background-color', "black")
